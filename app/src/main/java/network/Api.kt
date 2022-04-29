82f146f433b8fc6d57b9a2cea244e54b1e0daa3d
AnassAoulad <anass.aoulad.bh@gmail.com>
2022-04-29 11:23:45 +0200

    exercice 5

---
+++ app/src/main/java/network/Api.kt
@@ -0,0 +1,42 @@
+package network
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object Api {
+    private const val TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo3MjgsImV4cCI6MTY4MjcxMTUwM30.e9XfoQ2Ajl6XSR0Hr0IDdOUPr86qhjrw7Xa2Smvibdg"
+
+    private val retrofit by lazy<Any>{
+        // client HTTP
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor { chain ->
+                // intercepteur qui ajoute le `header` d'authentification avec votre token:
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $TOKEN")
+                    .build()
+                chain.proceed(newRequest)
+            }
+            .build()
+
+        // transforme le JSON en objets kotlin et inversement
+        val jsonSerializer = Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
+
+        // instance retrofit pour implémenter les webServices:
+        Retrofit.Builder()
+            .baseUrl("https://android-tasks-api.herokuapp.com/api/")
+            .client(okHttpClient)
+            .addConverterFactory(jsonSerializer.asConverterFactory("application/json".toMediaType()))
+            .build()
+        val userWebService : UserWebService by lazy<Any> {
+            retrofit.create(UserWebService::class.java)
+        }
+    }
+}
